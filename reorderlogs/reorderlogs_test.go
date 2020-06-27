db4b4535fd7c41bffd86a5b28074497851386c72
Anton Mednonogov <anton.mednonogov@ansarada.com>
2020-06-27 20:27:04 +0700

    Add a test case for ReorderLogFiles

--- reorderlogs/reorderlogs_test.go
+++ reorderlogs/reorderlogs_test.go
@@ -8,25 +8,27 @@ import (
 
 func TestReorderLogFiles(t *testing.T) {
 	// arrange
-	logs := []string{
-		"dig1 8 1 5 1",
-		"let1 art can",
-		"dig2 3 6",
-		"let2 own kit dig",
-		"let3 art zero"}
+	testCases := []struct {
+		logs, expectedResults []string
+	}{
+		{
+			[]string{"dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"},
+			[]string{"let1 art can", "let3 art zero", "let2 own kit dig", "dig1 8 1 5 1", "dig2 3 6"},
+		},
+		{
+			[]string{"8 fj dzz k", "5r 446 6 3", "63 gu psub", "5 ba iedrz", "6s 87979 5", "3r 587 01", "jc 3480612", "bb wsrd kp", "b aq cojj", "r5 6316 71"},
+			[]string{"b aq cojj", "5 ba iedrz", "8 fj dzz k", "63 gu psub", "bb wsrd kp", "5r 446 6 3", "6s 87979 5", "3r 587 01", "jc 3480612", "r5 6316 71"},
+		},
+	}
 
-	expectedResult := []string{
-		"let1 art can",
-		"let3 art zero",
-		"let2 own kit dig",
-		"dig1 8 1 5 1",
-		"dig2 3 6"}
+	for _, testCase := range testCases {
 
-	// act
-	result := ReorderLogFiles(logs)
+		// act
+		actualResult := ReorderLogFiles(testCase.logs)
 
-	// asset
-	if !cmp.Equal(expectedResult, result) {
-		t.Errorf("Expected %v. Actual %v", expectedResult, result)
+		// asset
+		if !cmp.Equal(testCase.expectedResults, actualResult) {
+			t.Errorf("Expected %v. Actual %v", testCase.expectedResults, actualResult)
+		}
 	}
 }
